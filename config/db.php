ed255b776d9643417984b62a996ff0e891e22d5a
ioramishvili <ioramishvili@yandex.ru>
2023-11-05 21:09:05 +0300

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => sprintf('mysql:host=%s;dbname=%s', 'shop-mysql', 'shop'),
+    'username' => 'shop',
+    'password' => 'p@ssw0rd',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
