bfd9c3141d6092c76f2dd7e47be88563ef127f2b
ioramishvili <ioramishvili@yandex.ru>
2023-11-06 11:31:22 +0300

    Подключение .env файла и изменения в README.md

--- config/db.php
+++ config/db.php
@@ -2,13 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => sprintf('mysql:host=%s;dbname=%s', 'shop-mysql', 'shop'),
-    'username' => 'shop',
-    'password' => 'p@ssw0rd',
+    'dsn' => sprintf('mysql:host=%s;dbname=%s', getenv('DB_HOST'), getenv('DB_NAME')),
+    'username' => getenv('DB_USERNAME'),
+    'password' => getenv('DB_PASSWORD'),
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
